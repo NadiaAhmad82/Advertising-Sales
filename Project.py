11cb91e99e8b1aab381c84d7b27cbd6c7328714a
NadiaAhmad82 <126454934+NadiaAhmad82@users.noreply.github.com>
2023-03-07 14:15:27 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-
+df = pd.read_csv('Advertising')
 st.write("""
 # Simple Prediction App
 This app predicts the **Advertising Sales** type!
 """)
 
 st.sidebar.header('User Input Parameters')
-
+st.write(df)
