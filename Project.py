579e7c1afdda020e673cf5042b05890e9e97ac3c
NadiaAhmad82 <126454934+NadiaAhmad82@users.noreply.github.com>
2023-03-07 15:03:14 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -12,9 +12,9 @@ st.sidebar.header('User Input Parameters')
 st.write(df)
 
 def user_input_features():
-    TV_Value = st.sidebar.slider('TV Value ', 0, 150, 300)
-    Newspaper_Value = st.sidebar.slider('Newspaper Value', 0, 70, 120)
-    Radio_Value = st.sidebar.slider('Radio Value', 0, 25, 50)
+    TV_Value = st.sidebar.slider('TV Value ', 0, 150, 500)
+    Newspaper_Value = st.sidebar.slider('Newspaper Value', 0, 150, 500)
+    Radio_Value = st.sidebar.slider('Radio Value', 0, 150, 200)
   
     data = {'TV_Value': TV_Value,
             'Newspaper_Value': Newspaper_Value,
@@ -26,12 +26,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-Advertising = datasets.load_Advertising.csv()
-X = Advertising.Sales
-Y = Advertising.target
 
-clf = RandomForestClassifier()
-clf.fit(X, Y)
 
 
 
