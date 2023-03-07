d2547e346b8b03f22ac53c17a6db964ee00a81c3
NadiaAhmad82 <126454934+NadiaAhmad82@users.noreply.github.com>
2023-03-07 14:23:55 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -11,11 +11,22 @@ This app predicts the **Advertising Sales** type!
 st.sidebar.header('User Input Parameters')
 st.write(df)
 
-X = Advertising.data
-Y = Advertising.target
+
+X = df.data
+Y = df.target
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
 
 prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+#st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
