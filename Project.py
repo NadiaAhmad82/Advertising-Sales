c146b9c9c06242838c1df75360b3d926df431dfb
NadiaAhmad82 <126454934+NadiaAhmad82@users.noreply.github.com>
2023-03-07 14:12:57 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -3,8 +3,6 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
-data = sns.load_dataset('Advertising')
-
 st.write("""
 # Simple Iris Flower Prediction App
 This app predicts the **Iris flower** type!
