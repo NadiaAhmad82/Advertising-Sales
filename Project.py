6d7f0645d53efaa5b61130ee9d181295f1f8937c
NadiaAhmad82 <126454934+NadiaAhmad82@users.noreply.github.com>
2023-03-07 14:32:03 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -10,23 +10,9 @@ This app predicts the **Advertising Sales** type!
 
 st.sidebar.header('User Input Parameters')
 st.write(df)
+from sklearn.naive_bayes import GaussianNB 
+model = GaussianNB()                      
+model.fit(Xtrain, ytrain)                  
+y_model = model.predict(Xtest)             
 
 
-X = df.data
-Y = df.target
-
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
